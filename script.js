b50af43017c3e5bec184307ece4a99bc08d49b22
Michał Baran <mbaran@polonez-plus.com.pl>
2026-03-05 16:03:16 +0100

    Refactor HTML structure and enhance JavaScript functionality for grid size selection and add event listener

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const container = document.querySelector(".container");
 const board = document.querySelector(".board");
+const gridSizeBtn = document.querySelector("#grid-size-btn");
 
-const size = 16;
-const cellCount = 16 * 16;
+let size = 16;
+const boardSize = 500;
+const cellSize = boardSize / size;
 
-for (let i = 0; i < cellCount; i++) {
-    const cell = document.createElement("div");
-    cell.className = "cell";
-    cell.dataset.index = i;
-    board.appendChild(cell);
-} 
+setCells(size);
+
+function setCells(size) {
+    const cellCount = size * size;
+    for (let i = 0; i < cellCount; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.dataset.index = i;
+        board.appendChild(cell);
+    }
+}
+
+function paint(cell) {
+    cell.style.backgroundColor = "#111";
+}
+
+board.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("cell")) return;
+    paint(e.target);
+    e.preventDefault();
+});
+
+board.addEventListener("mouseover", (e) => {
+    if (!e.target.classList.contains("cell")) return;
+    paint(e.target);
+    e.preventDefault();
+})
+
+gridSizeBtn.addEventListener("click", (e) => {
+    console.log("Enter btn");
+    size = prompt("Enter grid size 10-100");
+    setCells(size);
+});
